b5d24d4c3d575b1bcd396e817cc6da31d7def60a
VATSALA SAHU <vatsalasahu77@gmail.com>
2026-09-26 11:46:25 +0530

    Time: 0 ms (100%), Space: 87.1 MB (96.03%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+  
+    int five=0,ten=0;
+    for(int i=0;i< bills.size();i++){
+        if(bills[i]==5){ five++;}
+        else if(bills[i]==10){
+            five--;
+            ten++;
+            if(five<0)return false;
+        }
+        else{
+     if(ten > 0 and five > 0){
+        ten--;
+        five--;
+     }
+     else if(five>=3){five-=3;}
+     else return false;
+        }
+    }
+    return true;
+    }
+};
